d2b4ee72996ad575847e4cb8bb2f0ed7c56abf8a
codywelch <77134061+codywelch@users.noreply.github.com>
2021-04-12 15:01:59 -0400

    Added Dialogue
    Inherited interactable and attached component to an NPC initiate dialogue.
    Added UI for dialogue.

---
+++ AdventureGame/Assets/scripts/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DialogueTrigger : Interactable
+{
+    public Dialogue dialogue;
+
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("Tried to open dialogue");
+        TriggerDialoge();
+    }
+
+    void TriggerDialoge()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
